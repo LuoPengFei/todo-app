dbb0d3d24cc3b3006e145400f072b84f6e1894eb
骆朋飞 <lpf@luopengfei-2.local>
2017-03-26 16:01:48 +0800

    完善action

---
+++ src/action/TodoAction.js
@@ -0,0 +1,46 @@
+import TodoDispatcher form '../dispatcher/TodoDispatcher';
+import TodoConstant from '../constants/TodoConstant';
+
+let TodoAction = {
+  toggleItemList(id) {
+    TodoDispatcher.dispatch({
+      id,
+      type: TodoConstant.TOGGLEITEM
+    });
+  },
+
+  deleteItem(id) {
+    TodoDispatcher.dispatch({
+      id,
+      type: TodoConstant.DELETEITEM
+    });
+
+  },
+
+  createItem(title) {
+    TodoDispatcher.dispatch({
+      title,
+      type:TodoConstant.CREATEITEM
+    });
+  },
+
+  editItem(id, title) {
+    TodoDispatcher.dispatch({
+      id,
+      title,
+      type: TodoConstant.EDITITEM
+    });
+  },
+
+  loadData() {
+    fetch("todo.json")
+    .then((data)=>data.json())
+    .then((todos)=>{
+      TodoDispatcher.dispatch({
+        todos,
+        type: TodoConstant.LOADDATA
+      });
+      // this.setState({todos});
+    })
+  }
+}
---
+++ src/constants/TodoConstant.js
@@ -0,0 +1,10 @@
+
+let TodoConstant = {
+  TOGGLEITEM: 'toggleItem',
+  DELETEITEM: 'deleteItem',
+  CREATEITEM: 'createItem',
+  EDITITEM: 'editItem',
+  LOADDATA: 'loadData'
+}
+
+module.exports = TodoConstant;
